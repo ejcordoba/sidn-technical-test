1533527aaa16af45ff579d7d0187a21d90801581
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-04-08 11:29:31 +0200

    Add footer to first static version

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
 
+<div class="footer">
+    <img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/src/img/footer-logo.png" />
+    <img class="footer-separator" src="<?php echo get_template_directory_uri(); ?>/src/img/footer-separator.png" />
+    <div class="legal-notice-legal-information">
+        Legal notice | Legal information
+    </div>
+</div>
 <?php wp_footer();?>
 </body>
 </html>
